99718a2c2b230a4dab8729e691d31d97aa9e0654
liljepim <misagaljustinpaul@gmail.com>
2025-08-06 02:36:53 +0800

    Major and Minor Changes Major Changes: Added CSV generation for the language models (default=70,000 words) Minor Changes: Modified .gitignore to not include CSV files in tracking

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import re
 import string
 
@@ -30,6 +31,18 @@ def generate_report(filename):
     return [words, word_count]
 
 
+def generate_csv(language, word_list, word_count):
+    headers = ["Word", "Frequency Count", "Probability (%)"]
+    filename = f"{language}_model.csv"
+    with open(filename, "w") as f:
+        fw = csv.writer(f)
+        fw.writerow(headers)
+        for word in word_list:
+            freq = word_list[word]
+            prob = (freq / word_count) * 100
+            fw.writerow([word, freq, f"{prob:.6f}%"])
+
+
 def format_value(val):
     if isinstance(val, float) and val != 0 and abs(val) < 0.001:
         return f"{val:.2e}"  # scientific notation
@@ -46,11 +59,14 @@ def main():
     alpha = 0.01
     data = []
     tot_nb = 0
+    csv_data = []
     for file in file_list:
         curr_lang = file.split(".")[0]
         words, lang_count = generate_report(file)
+        generate_csv(curr_lang, words, lang_count)
         lang_probs[curr_lang] = {"word_count": lang_count, "word_list": words}
         total_words += lang_count
+
     for lang in list(lang_probs.keys()):
         stats[lang] = {}
         stats[lang][f"P({lang})"] = lang_probs[lang]["word_count"] / total_words
@@ -94,10 +110,10 @@ def main():
         "Naive Bayes Result",
     ]
     print(tabulate(data, headers=headers, tablefmt="grid"))
-    print("Normalized Results:")
+    print("\nNormalized Results:")
     for lang in list(stats.keys()):
         print(
-            f"{lang.capitalize()}: {((stats[lang]['naive_bayes'] / tot_nb) * 100):.2f}% {'<---- Answer' if lang == best_lang[0] else ''}"
+            f"{lang.capitalize()}: {((stats[lang]['naive_bayes'] / tot_nb) * 100):.2f}% {'\t\033[1;3;92m<---- Answer\033[0m' if lang == best_lang[0] else ''}"
         )
 
 
